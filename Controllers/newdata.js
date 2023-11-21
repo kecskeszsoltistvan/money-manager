4a70bd4a16bcb7f48b8b3ca9fec8085769fb2494
kecskeszsoltistvan <kecskeszsoltistvan@turr.hu>
2023-11-21 16:47:51 +0100

    Az egész végre kész (adat beolvasás)

--- Controllers/newdata.js
+++ Controllers/newdata.js
@@ -20,10 +20,10 @@ function TagLoader(){
                 tipusok.appendChild(opcio);
             })
         })
-    }, 100);
+    }, 500);
     }
 
-function addItems(){
+function addItems(){ // Ez az egész egy kín szenvedés volt, lehet egy kicsit hosszú is, de legalább működik, úgyhogy kit érdekel.
 
     let date = document.querySelector('#date');
     let type = document.querySelector('#type');
@@ -36,47 +36,106 @@ function addItems(){
     }
     else{
         console.log(`Má' megint kezdődik`);
+        let vane = false;
+        let upID = -1;
         axios.get(`${serverURL}/items/userID/eq/${loggedUser.ID}`).then(res=>{
-            let vane = false;
-            let upID = -1;
-            console.log(`Generálás...`);
+            console.log("Csere keresés...");
             res.data.forEach(item => {
-            let cucc = item
-                axios.get(`${serverURL}/catgs/`).then(x=>{
-                    if (item.date.split('T')[0] == date.value){
-                        vane = true;
-                        upID = item.ID;
-                        return;
-                    }
+            if (item.userID == loggedUser.ID && item.date.split('T')[0] == date.value && item.typeID == type.value){
+                axios.get(`${serverURL}/catgs/`).then(catDATA => {
+                    console.log(catDATA);
+                    catDATA.data.forEach(thing => {
+                        console.log(thing.ID, item.tagID);
+                        console.log(thing.ID == item.tagID);
+                        if (thing.ID == item.tagID){
+                            upID = item.ID
+                            vane = true
+                            return;
+                        }
+                    })
                 })
-            });
-            if(vane){
+            }
+            })
+        })
+
+        setTimeout(()=>{
+            if (vane){
                 let data = {
-                    items : items.value	
+                    amount : items.value
                 }
-                axios.patch(`${serverURL}/items/ID/eq/${upID}`, data).then((res)=>{
-                    alert('Az érték módosítva!');
+                axios.patch(`${serverURL}/items/ID/eq/${upID}`, data).then(req =>{
                     date.value = null;
                     items.value = 0;
-                });
+                    custom_tipusok.value = "";
+                })
+                alert("Adat sikeresen frissítve!");
             }
-            else{
-                if (item.date.split('T')[0] == date.value){
-                    let data = {
-                        userID : loggedUser.ID,	
-                        date : date.value,	
-                        typeID : x.ID,
-                        amount : items.value,	
-                    }
+            else if (!vane){
+                let newData = {
+                    userID : loggedUser.ID,
+                    date : date.value,
+                    typeID : type.value,
+                    amount : items.value,
+                    tagID : null
+                }
+                console.log(`${custom_tipusok.value.length == 0 ? tipusok.value : custom_tipusok.value}`);
+                if (custom_tipusok.value.length == 0){ // Ha nem saját kategóriát adunk meg
+                    axios.get(`${serverURL}/catgs/`).then(catDATA => {
+                        catDATA.data.forEach(catInstance =>{
+                            if (catInstance.tagname == tipusok.value){
+                                newData.tagID = catInstance.ID;
+                                axios.post(`${serverURL}/items/`, newData).then(req =>{
+                                    date.value = null;
+                                    items.value = 0;
+                                    custom_tipusok.value = "";
+                                })
+                                alert("Adat sikeresen rögzítve!");
+                            }
+                        }
+                        )
+                    })
+                }
+                else{
+                    axios.get(`${serverURL}/catgs/`).then(catDATA => {
+                        let vanilyen = false
+                        catDATA.data.forEach(catInstance =>{
+                            if (catInstance.tagname == custom_tipusok.value){ // Ha már létezik ez a kategória, ne adjuk a másik táblához megint.
+                                newData.tagID = catInstance.ID;
+                                axios.post(`${serverURL}/items/`, newData).then(req =>{
+                                    date.value = null;
+                                    items.value = 0;
+                                    custom_tipusok.value = "";
+                                })
+                                alert("Adat sikeresen rögzítve!");
+                                vanilyen = true;
+                                return;
+                            }
+                        })
+                        if (!vanilyen){
     
-                    axios.post(`${serverURL}/items`, data).then((res)=>{
-                        alert('A lépésszám rögzítve!');
-                        date.value = null;
-                        items.value = 0;
-                    });
+                            let newCat = { // Ha nem találta, akkor hozzáadjuk az új kategóriát
+                                tagname : custom_tipusok.value
+                            }
+                            axios.post(`${serverURL}/catgs/`, newCat).then(cucmákocska => {
+                                catDATA.data.forEach(catInstance =>{
+                                    if (catInstance.tagname == custom_tipusok.value){ 
+                                        newData.tagID = catInstance.ID;
+                                        axios.post(`${serverURL}/items/`, newData).then(req =>{
+                                            date.value = null;
+                                            items.value = 0;
+                                            custom_tipusok.value = "";
+                                        })
+                                        alert("Adat sikeresen rögzítve!");
+                                        return;
+                                    }
+                                })
+                            })
+                        }
+                    })
                 }
             }
-        })
+        }, 100)
+        
     }
 }
 
