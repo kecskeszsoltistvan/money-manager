1c176862f959cea9122df541be6e1f93f56963b7
kecskeszsoltistvan <kecskeszsoltistvan@turr.hu>
2023-11-24 09:01:42 +0100

    Táblázat működik

--- Controllers/table.js
+++ Controllers/table.js
@@ -2,7 +2,7 @@ function getAllSteps(){
     let tbody = document.querySelector('tbody');
     let sumSteps = document.querySelector('#sumSteps');
 
-    axios.get(`${serverURL}/steps/userID/eq/${loggedUser.ID}`).then(res=>{
+    axios.get(`${serverURL}/items/userID/eq/${loggedUser.ID}`).then(res=>{
         let i = 0;
         let sum = 0;
         res.data.sort((a,b) => a.date.localeCompare(b.date));
@@ -13,13 +13,23 @@ function getAllSteps(){
             let td3 = document.createElement('td');
 
             i++;
-            sum += item.steps;
+            if (item.typeID == 1) {
+                sum += item.amount;
+            }
+            else{
+                sum -= item.amount;
+            }
+            
 
             td1.innerHTML = i + '.';
 
             td2.innerHTML = item.date.split('T')[0];
-
-            td3.innerHTML = item.steps;
+            if (item.typeID == 1) {
+                td3.innerHTML = item.amount;
+            }
+            else{
+                td3.innerHTML = -item.amount;
+            }
             td3.classList.add('text-end');
 
             tr.appendChild(td1);
